6fddad714c17a8c1c44a3d1b8e53b5c0582a2b13
junnystateofmind <jun1557@snu.ac.kr>
2024-03-27 21:39:18 +0900

    20240327 CNN 3층 구조에서 2번째 층부터  Depthwise Pointwise 적용, 최대한 많은 필터를 적용하려 함

--- models/cnn.py
+++ models/cnn.py
@@ -1,16 +1,47 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
+# Overview
+# 10 classes: airplane, bird, car, cat, deer, dog, horse, monkey, ship, truck.
+# Images are 96x96 pixels, color.
+# 500 training images (10 pre-defined folds), 800 test images per class.
+# 100000 unlabeled images for unsupervised learning. These examples are extracted from a similar but broader distribution of images. For instance, it contains other types of animals (bears, rabbits, etc.) and vehicles (trains, buses, etc.) in addition to the ones in the labeled set.
+# Images were acquired from labeled examples on ImageNet.
 
 def CNN(input_shape=(96, 96, 3), num_classes=10):
     model = models.Sequential([
-        layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
-        layers.MaxPooling2D((2, 2)),
-        layers.Conv2D(64, (3, 3), activation='relu'),
-        layers.MaxPooling2D((2, 2)),
-        layers.Conv2D(64, (3, 3), activation='relu'),
-        layers.Flatten(),
-        layers.Dense(64, activation='relu'),
+        layers.Conv2D(kernel_size=(3,3), filters=64, activation='relu', input_shape=input_shape), # 로컬 패턴과 질감, 기본적인 형태를 감지하는 데 중점을 두기 위해 64개의 커널을 사용, 94x94x64
+        layers.MaxPooling2D((2, 2)), # 풀링 레이어를 추가, 차원은 47x47x64
+        layers.Conv2D(kernel_size=(3,3), filters=256, activation='relu'), # 커널 수 256개로 늘려 세부적인 패턴을 감지, 45x45x256
+        # depthwise separable convolution을 통해 모델의 크기를 줄이고, 계산량을 줄이기 위해 사용
+        layers.DepthwiseConv2D(kernel_size=(3,3), activation='relu'), # 43x43x256
+        # pointwise convolution을 통해 depthwise separable convolution의 출력을 다시 합쳐줌
+        layers.Conv2D(filters=256, kernel_size=(1,1), activation='relu'), # 43x43x256
+        # 풀링 레이어를 추가
+        layers.MaxPooling2D((2, 2)), # 21x21x256
+        # 한번 더 depthwise separable convolution을 적용
+        layers.DepthwiseConv2D(kernel_size=(3,3), activation='relu'), # 19x19x256
+        layers.Conv2D(filters=256, kernel_size=(1,1), activation='relu'), # 19x19x256
+        layers.GlobalAveragePooling2D(), # 256
+        layers.Dense(128, activation='relu'), # 128
+        layers.Dense(num_classes) # 10
+    ])
+    return model
+
+
+
+def SimpleCNN(input_shape=(96, 96, 3), num_classes=10):
+    model = models.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape), # 94x94x32
+        layers.MaxPooling2D((2, 2)), # 47x47x32
+        layers.Conv2D(64, (3, 3), activation='relu'), # 45x45x64
+        layers.MaxPooling2D((2, 2)), # 22x22x64
+        layers.Conv2D(64, (3, 3), activation='relu'), # 20x20x64
+        layers.Conv2D(64, (3, 3), activation='relu'), # 18x18x64
+        layers.Flatten(), # 20736
+        layers.Dense(64, activation='relu'), #
         layers.Dense(num_classes)
     ])
     return model
+# 모델 요약
+
--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,27 @@
-#utils.py
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+import logging
+
+def setup_logging(log_file='training.log'):
+    logging.basicConfig(filename=log_file, level=logging.INFO)
+
+def log_message(message):
+    logging.info(message)
+
+def create_data_augmentation_generator(): # 데이터 증강 생성기
+    return ImageDataGenerator(
+        rotation_range=20, # 이미지 회전
+        width_shift_range=0.2, # 가로 이동
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest'
+    )
+
+def calculate_performance_metrics(y_true, y_pred):
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred, average='macro')
+    recall = recall_score(y_true, y_pred, average='macro')
+    return accuracy, precision, recall
+
