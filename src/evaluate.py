9fe18cbb024321af0200c46724adee4cbb772295
junnystateofmind <jun1557@snu.ac.kr>
2024-04-03 19:35:23 +0900

    evaluate 구현

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,14 +2,24 @@ import tensorflow as tf
 from tensorflow.keras import datasets
 from .train import load_and_preprocess_data
 import os
+import argparse
 
 # 데이터 로드 및 전처리
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='cnn', help='Model to evaluate (cnn or efficientnet)')
+
     test_images, test_labels = load_and_preprocess_data()
 
+    args = parser.parse_args()
     # 모델 로드
-    model = tf.keras.models.load_model('model/cnn.h5')
+    if args.model == 'CNN':
+        model = tf.keras.models.load_model('models/cnn.h5')
+    elif args.model == 'EfficientNet':
+        model = tf.keras.models.load_model('models/efficientnet.h5')
+    else:
+        raise ValueError('Unknown model type: {}'.format(args.model))
 
     # TensorBoard 로그 디렉토리 설정
     log_dir = os.path.join('logs', 'evaluate')
